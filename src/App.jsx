ddf937fd568b76d1e2d7fb030714850a6b5a6e91
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-10-25 21:14:37 -0400

    Changed standardMaterial to phongMaterial in order to create a car in blender and import it

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React from "react"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, render, events } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 
 const App = () => {
+	window.addEventListener("resize", () =>
+		render(<mesh />, document.querySelector("canvas"), {
+			events,
+			size: { width: window.innerWidth, height: window.innerHeight },
+		})
+	)
+
 	return (
 		<div id='canvas-container'>
-			<Canvas dpr={[1, 2]} camera={{ position: [-10, 0, 0], fov: 35 }}>
+			<Canvas
+				dpr={[1, 2]}
+				camera={{ fov: 75, near: 0.1, far: 100, position: [0, 0, 5] }}>
 				<mesh>
 					<ambientLight intensity={0.5} position={[4, 7, 7]} />
 					<directionalLight color='white' position={[2, 5, 5]} />
 					<sphereGeometry args={[1, 32, 32]} />
-					<meshStandardMaterial color={"yellow"} />
+					<meshPhongMaterial color={"royalblue"} />
 					<OrbitControls
 						autoRotate
 						enablePan={true}
-						enableZoom={false}
+						enableZoom={true}
 						enableRotate={true}
 						minPolarAngle={Math.PI / 2.8}
 						maxPolarAngle={Math.PI / 2.8}
@@ -25,5 +34,6 @@ const App = () => {
 		</div>
 	)
 }
+window.dispatchEvent(new Event("resize"))
 
 export default App
