efe968549b2b4cdd22c5d6715b402d993c2818bc
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-10-31 19:08:56 -0400

    New Car

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Canvas, render, events } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 
@@ -14,23 +14,7 @@ const App = () => {
 		<div id='canvas-container'>
 			<Canvas
 				dpr={[1, 2]}
-				camera={{ fov: 75, near: 0.1, far: 100, position: [0, 0, 5] }}>
-				<mesh>
-					<ambientLight intensity={0.5} position={[4, 7, 7]} />
-					<directionalLight color='white' position={[2, 5, 5]} />
-					{/* <sphereGeometry args={[1, 32, 32]} /> */}
-					{/* <meshPhongMaterial color={"royalblue"} /> */}
-					<OrbitControls
-						autoRotate
-						enablePan={true}
-						enableZoom={true}
-						enableRotate={true}
-						minPolarAngle={Math.PI / 2.8}
-						maxPolarAngle={Math.PI / 2.8}
-						enableDamping={true}
-					/>
-				</mesh>
-			</Canvas>
+				camera={{ fov: 75, near: 0.1, far: 100, position: [0, 0, 5] }}></Canvas>
 		</div>
 	)
 }
