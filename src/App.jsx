33a253f52af8d57f9d86b4ea984776bb04bb0ada
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-10-31 22:38:40 -0400

    AMG model added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react"
 import { Canvas, render, events } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, Stage } from "@react-three/drei"
+import Model from "./Model"
 
 const App = () => {
 	window.addEventListener("resize", () =>
@@ -13,8 +14,20 @@ const App = () => {
 	return (
 		<div id='canvas-container'>
 			<Canvas
+				shadows
 				dpr={[1, 2]}
-				camera={{ fov: 75, near: 0.1, far: 100, position: [0, 0, 5] }}></Canvas>
+				camera={{ fov: 75, near: 0.1, far: 100, position: [0, 0, 5] }}>
+				<Suspense fallback={null}>
+					<Stage
+						environment={null}
+						intensity={1}
+						contactShadowOpacity={1}
+						shadowBias={-0.0015}>
+						<Model />
+					</Stage>
+					<OrbitControls />
+				</Suspense>
+			</Canvas>
 		</div>
 	)
 }
