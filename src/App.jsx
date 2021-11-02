c2e4a612e038006ba3718c85362620b7cc296e37
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-11-02 18:21:18 -0400

    Bug on gltf, fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, render, events } from "@react-three/fiber"
 import { OrbitControls, Stars } from "@react-three/drei"
 import Model from "./Model"
 
