1e44cc5f94c808962c878549f65fc1d1d590af99
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-11-13 09:08:49 -0500

    build ready

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -5,14 +5,18 @@ export default {
 		public: "/",
 		src: "/dist",
 	},
-	plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-sass"],
+	plugins: [
+		"@snowpack/plugin-react-refresh",
+		"@snowpack/plugin-sass",
+		"@snowpack/plugin-webpack",
+	],
 	routes: [
 		/* Enable an SPA Fallback in development: */
 		// {"match": "routes", "src": ".*", "dest": "/index.html"},
 	],
 	optimize: {
 		/* Example: Bundle your final build: */
-		// "bundle": true,
+		bundle: true,
 	},
 	packageOptions: {
 		/* ... */
